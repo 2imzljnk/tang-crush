059fc59ed3284e2371764a9212578e4610b7b2fc
2imzljnk <86717485+2imzljnk@users.noreply.github.com>
2021-06-30 19:28:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'c iuuu ahhh!',
+    introDesc: `1 voi 1 la high
+    2 them 2 la bon'
+    4 voi 1 la 5
+   la gi cung dc mien c cute la chap nhan het akk  :3`,
+    btnIntro: 'hjhjhj =))))',
+    title: 'doan nay em lười chỉnh wa nên hok coa gi douuu =))))',
+    desc: 'doan xem em se chon gi ik :>  ',
+    btnYes: 'chọn em neeee',
+    btnNo: 'o kia bảo chọn em màaaaaa :< ',
+    question:'chốt lại cóa iu em hokkk =))))',
+    btnReply: 'nhanh len hok la em biến mất neeeeeee uwu ',
+    reply: 'chi se chon em ma =))))))',
+    mess: 'em bic ngay maa =)))) kjkjkj ',
+    messDesc: 'toi nay vao ban pubg nkiaaaa =)))) .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
